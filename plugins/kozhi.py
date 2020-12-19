f9c4171a75f84869458129c312b898edf93b345d
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-12-19 12:54:41 +0530

    Update kozhi.py

--- plugins/kozhi.py
+++ plugins/kozhi.py
@@ -9,14 +9,13 @@ from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
 async def tip(bot, update):
         # logger.info(update)
 
-    await bot.send_photo(
+    await bot.send_message(
         chat_id=update.chat.id,
         caption = """Hello,
 This is a Telegram URL Upload Bot!
 <b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
 /help for more details..
 Support Group :@TGB_Support""",
-        photo ="AgADBQADN6sxGwLcmFaIdff06SZuGI386m50AAMBAAMCAAN5AAPZVQACFgQ",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="DEV", url="https://t.me/Pr_o_To")],
                                                     [InlineKeyboardButton(text="GROUP", url="https://t.me/TGB_Support"),
                                                      InlineKeyboardButton(text="CHANNNEL", url="http://t.me/TG_BotZ")]]),
